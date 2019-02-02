687c27b993fdf0e4518213567461724a57a607ec
Nicholas Tierney <nicholas.tierney@gmail.com>
2019-02-02 14:20:42 -0800

    convert the stan mcmc_to_dt tidier into data.table

--- R/mcmc_tidier_stan.R
+++ R/mcmc_tidier_stan.R
@@ -9,25 +9,44 @@
 #'
 mcmc_to_dt.stanfit <- function(mcmc_object, ...){
 
-    samples_data <- purrr::map(mcmc_object@sim$samples, as.data.frame)
+    samples_data <- data.table::rbindlist(mcmc_object@sim$samples)
 
     n_chain <- mcmc_object@sim$chains
+
     n_iter <- mcmc_object@sim$iter
 
-    # convert to data.table
-    tidy_fit_stan <- samples_data %>%
-        dplyr::bind_rows() %>%
-        tibble::as_tibble() %>%
-        dplyr::mutate(chain = rep(1:n_chain, each = n_iter),
-                      iteration = rep(1:n_iter, 4)) %>%
-        tidyr::gather(key = "parameter",
-                      value = "value",
-                      -chain,
-                      -iteration) %>%
-        dplyr::select(iteration,
-                      chain,
-                      parameter,
-                      value)
-    tidy_fit_stan
+    tidy_fit_dt <-
+        data.table::data.table(
+            samples_data, # drop lp__
+            iteration = rep(1:n_iter, 4),
+            chain = rep(1:n_chain, each = n_iter)
+        )
+
+    dt_melt <- data.table::melt.data.table(
+        data = tidy_fit_dt,
+        id.vars = c("iteration",
+                    "chain"))
+
+    data.table::setnames(dt_melt, c("iteration",
+                                    "chain",
+                                    "parameter",
+                                    "value"))
+
+    # change the order of the columns
+    data.table::setcolorder(dt_melt, c("iteration",
+                                       "chain",
+                                       "parameter",
+                                       "value"))
+
+    # arrange the row order
+    data.table::setorder(dt_melt,
+                         parameter,
+                         chain,
+                         iteration)
+
+
+    dt_melt
+
+
 
 }
