43d72703eb99d2516c3bca2dd154bcd753cf6c45
Nicholas Tierney <nicholas.tierney@gmail.com>
2019-02-03 17:23:36 -0800

    remove rstan model generating code, as it has been compacted into example_stan_model

--- R/data-example-stan-model.R
+++ R/data-example-stan-model.R
@@ -8,21 +8,6 @@
 #' @usage data(example_stan_model)
 #' @keywords data
 #' @examples
-#' \dontrun{
-#'     library(rstan)
-#'     scode <- "
-#'     parameters {
-#'     real y[2];
-#'     }
-#'     model {
-#'     y[1] ~ normal(0, 1);
-#'     y[2] ~ double_exponential(0, 2);
-#'     }
-#'     "
-#'     example_stan_model <- stan(model_code = scode,
-#'                                iter = 10,
-#'                                verbose = FALSE)
-#' }
 #'
 #' mcmc_to_dt(example_stan_model)
 "example_stan_model"
