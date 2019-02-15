db8e2815542a73e79b3f68086ea082186b350fd8
Nicholas Tierney <nicholas.tierney@gmail.com>
2019-02-15 13:55:07 +1000

    complete tests for n_chain

--- tests/testthat/test-n-chain.R
+++ tests/testthat/test-n-chain.R
@@ -12,3 +12,22 @@ test_that("n_chain returns integer", {
     expect_is(n_chain(example_stan_model), "integer")
     expect_is(n_chain(example_jags_model), "integer")
 })
+
+test_that("n_chain returns NULL", {
+    expect_null(n_chain(NULL))
+})
+
+test_that("n_chain returns error when class not known", {
+    expect_error(n_chain(iris))
+    expect_error(n_chain(100))
+    expect_error(n_chain("wat"))
+})
+
+test_that("n_chain returns one number only", {
+    expect_length(n_chain(line), 1)
+    expect_length(n_chain(line$line1), 1)
+    expect_length(n_chain(mcmc_to_dt(line)), 1)
+    # expect_length(n_chain(mcmc_to_dt(line$line1)), 1)
+    expect_length(n_chain(example_stan_model), 1)
+    expect_length(n_chain(example_jags_model), 1)
+})
